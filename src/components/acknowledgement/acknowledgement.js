49766db158cb5ef35a835c3516ed58f770cf84f8
astrobot-me <72189934+astrobot-me@users.noreply.github.com>
2025-08-11 14:23:09 +0530

    Ack Text Update

--- src/components/acknowledgement/acknowledgement.js
+++ src/components/acknowledgement/acknowledgement.js
@@ -5,9 +5,7 @@ const Acknowledgement = () => {
     <div className="acknowledgement">
       <div className="ack-main">
         <h1>Acknowledgement</h1>
-        <p>The <b>Microsoft CMT</b> service was used for managing the peer-reviewing process for this conference. 
-          This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services
-          as well as for software development and support.</p>
+        <p>The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
         </div>
 
     </div>
