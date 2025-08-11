1d1edcfab9ce5174c84333cb7a78e94b664f85a2
astrobot-me <72189934+astrobot-me@users.noreply.github.com>
2025-08-11 14:26:53 +0530

    Ack Section Updated

--- src/components/acknowledgement/acknowledgement.js
+++ src/components/acknowledgement/acknowledgement.js
@@ -4,7 +4,9 @@ const Acknowledgement = () => {
   return (
     <div className="acknowledgement">
       <div className="ack-main">
-        <h1>Acknowledgement</h1>
+        <h1 style={{
+          textTransform:"uppercase"
+        }}>CMT Acknowledgement</h1>
         <p>The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
         </div>
 
